5d0e4f9d3d021c661721025f5bbc6db18824afa5
kk0829 <244098979@qq.com>
2024-02-01 08:22:16 +0800

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by 王干 on 2024/2/1.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
